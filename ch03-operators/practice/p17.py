54351d7277f5f3cfb2285a05981a3cb7f5c42c6a
GEUNHYEONG <gim35182@g.yju.ac.kr>
2026-08-17 22:16:47 +0900

    20206-08-17 다중 대입

--- ch03-operators/practice/p17.py
+++ ch03-operators/practice/p17.py
@@ -12,8 +12,32 @@
 """
 
 # 아래에 코드를 작성하세요
+# 과목 종류
+sub1, sub2, sub3 = "국어", "영어", "수학"
 
+# 각 점수를 입력받기
+lan = int(input("국어 점수: "))
+eng = int(input("영어 점수: "))
+math = int(input("수학 점수: "))
 
+# 초기화
+total = 0
+
+# 총점 계산
+total += lan
+total += eng
+total += math
+
+# 평균 계산
+average = total / 3
+
+# 출력
+print(f"과목: {sub1} {sub2} {sub3}")
+print(f"{sub1}: {lan}")
+print(f"{sub2}: {eng}")
+print(f"{sub3}: {math}")
+print(f"총점: {total}")
+print(f"평균: {average}")
 """
 [실행 결과 예시] (입력: 85, 90, 78)
 과목: 국어 영어 수학
