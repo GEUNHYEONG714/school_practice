1b5bc07a40cf46ed6d67bab5f2e276086ffd3e65
GEUNHYEONG <gim35182@g.yju.ac.kr>
2026-08-25 21:45:07 +0900

    2026-08-25 if-elif-else 조건문 연습

--- ch04-flow-control/practice/03_if_elif_else/p01.py
+++ ch04-flow-control/practice/03_if_elif_else/p01.py
@@ -17,8 +17,26 @@ if/elif/else를 사용합니다.
 score = int(input("점수를 입력하세요: "))
 
 # 아래에 등급을 판정하여 출력하세요
+level = ""
+# 90점 이상
+if score >= 90:
+    level = "A"
+# 80 이상: B
+elif score >= 80:
+    level = "B"
+# 70 이상: C
+elif score >= 70:
+    level = "C"
+# 60 이상: D
+elif score >= 60:
+    level = "D"
+# 60 미만: F
+else:
+    level = "F"
 
-
+# 출력
+print(f"점수: {score}")
+print(f"등급: {level}")
 """
 [실행 결과 예시] (입력: 85)
 점수: 85
